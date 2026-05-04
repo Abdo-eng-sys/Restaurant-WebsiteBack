5367811bb6e9372ae1f101e767b39e50bb0c1836
Abd Ul-Rahman <=>
2026-05-05 00:28:02 +0300

    First Edit

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,6 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    // 'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
     'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
